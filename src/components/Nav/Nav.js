6c74d0ef6e98174792573d791161032c3a7292aa
OneWay-Laye <tayloralaye@gmail.com>
2022-06-03 01:02:48 -0400

    This initial commit will create the skeleton for the Nav

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+
+// import styles
+import './Nav.scss'
+
+function Nav() {
+  return(
+    <div id='mainNav'>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About Me</a></li>
+        <li><a href='#'>Projects</a></li>
+      </ul>
+    </div>
+  )
+}
+
+
+export default Nav
