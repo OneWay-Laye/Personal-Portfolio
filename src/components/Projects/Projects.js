22eb5d2ca2b9852d8ad64740b52b5336f58d0edb
Alaye Taylor <tayloralaye@gmail.com>
2022-06-05 02:12:22 -0400

    This commit will add the list of projects ive done

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,34 @@
+import './Projects.scss'
+
 function Projects() {
+  let projects = [
+      {
+        name: "Neon Tic Tac Toe",
+        webLink: 'https://oneway-laye.github.io/Tic-Tac-Toe-client/',
+        repoLink: 'https://github.com/OneWay-Laye/Tic-Tac-Toe-client',
+        pictureLink: 'https://i.imgur.com/krz5kbV.png'
+      },
+      {
+        name: "Budget Tracker",
+        webLink: 'https://oneway-laye.github.io/Budget-Tracker-Frontend/',
+        repoLink: 'https://github.com/OneWay-Laye/Budget-Tracker-Frontend',
+        pictureLink: 'https://i.imgur.com/mMqHLDK.png'
+      },
+      {
+        name: "ECommerce Store - Donuts R Us",
+        webLink: 'https://dom-inators.github.io/ecommerce-frontend-client/',
+        repoLink: 'https://github.com/DOM-inators/ecommerce-frontend-client',
+        pictureLink: 'https://i.imgur.com/jOyr2Lw.png'
+      }
+    ]
+  let projectsJsx = ''
+
+
+
   return(
     <div>
-      <h1>I am in Projects
-    </h1>
+      {projectsJsx}
+      {console.log(projects)}
     </div>
   )
 }
