414c5d66a220a751af362cbe5b5e27a0a8a7823f
Alaye Taylor <tayloralaye@gmail.com>
2022-06-04 23:15:31 -0400

    This commit will add Contact Form

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,7 +1,37 @@
+import './ContactMe.scss'
+
 function ContactMe() {
   return(
-    <div>
-      <h1>I am in Contact Me</h1>
+    <div id="contactContainer">
+      <section id="contactCard">
+        <form id='contactForm'>
+          <label>First name</label>
+          <input
+            placeholder="Enter your first name"
+          />
+
+          <label>Last name</label>
+          <input
+            placeholder="Enter your last name"
+          />
+
+          <label>Email</label>
+          <input
+            type='email'
+            placeholder="Enter your email"
+          />
+          
+          <label>Message</label>
+          <input
+            placeholder="Enter the message"
+          />
+
+          <button>Send Message</button>
+        </form>
+      </section>
+      <section id='contactSocial'>
+
+      </section>
     </div>
   )
 }
