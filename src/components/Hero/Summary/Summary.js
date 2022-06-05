6ed6209cc5475e19fb4790f1a1bd035f7d54f0bf
Alaye Taylor <tayloralaye@gmail.com>
2022-06-04 21:33:29 -0400

    This commit finishes the layout and font-size

--- src/components/Hero/Summary/Summary.js
+++ src/components/Hero/Summary/Summary.js
@@ -3,7 +3,7 @@ import './Summary.scss'
 function Summary() {
   return(
     <div id='heroSummary'>
-      <h2>Alaye Taylor</h2>
+      <h1>Alaye Taylor</h1>
       <p>An Atlanta based Software Engineer</p>
     </div>
   )
