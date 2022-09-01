61e06801013a18f8ccd183ed1742a01adb88ec9e
Alaye Taylor <tayloralaye@gmail.com>
2022-08-31 22:44:48 -0400

    Random

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
   return(
     <div id='mainNav'>
       <ul>
-        <li class='home'><a href='#'>Home</a></li>
+        <li><a href='#'>Home</a></li>
         <li><a href='#'>About Me</a></li>
         <li><a href='#'>Projects</a></li>
         <li><a href='#'>Contact Me</a></li>
