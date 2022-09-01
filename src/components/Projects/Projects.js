3b6d3cffab7759b3ba5bb77fb66ff74a5398c009
Alaye Taylor <tayloralaye@gmail.com>
2022-08-31 20:48:12 -0400

    This Commit will update my project section to give give comments on each project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,25 +6,33 @@ function Projects() {
         name: "Neon Tic Tac Toe",
         webLink: 'https://oneway-laye.github.io/Tic-Tac-Toe-client/',
         repoLink: 'https://github.com/OneWay-Laye/Tic-Tac-Toe-client',
-        pictureLink: 'https://i.imgur.com/krz5kbV.png'
+        pictureLink: 'https://i.imgur.com/krz5kbV.png',
+        comments: 'This was my first project. I created a two player Tic-Tac-Toe game where the user had to have an account to play.',
+        toolsUsed: 'Javascript, Jquery, and CSS'
       },
       {
         name: "Budget Tracker",
         webLink: 'https://oneway-laye.github.io/Budget-Tracker-Frontend/',
         repoLink: 'https://github.com/OneWay-Laye/Budget-Tracker-Frontend',
-        pictureLink: 'https://i.imgur.com/mMqHLDK.png'
+        pictureLink: 'https://i.imgur.com/mMqHLDK.png',
+        comments: '',
+        toolsUsed: ''
       },
       {
         name: "ECommerce Store - Donuts R Us",
         webLink: 'https://dom-inators.github.io/ecommerce-frontend-client/',
         repoLink: 'https://github.com/DOM-inators/ecommerce-frontend-client',
-        pictureLink: 'https://i.imgur.com/jOyr2Lw.png'
+        pictureLink: 'https://i.imgur.com/jOyr2Lw.png',
+        comments: '',
+        toolsUsed: ''
       }
     ]
   let projectsJsx = projects.map(project => (
     <div className="projectCard">
       <img src={project.pictureLink}/>
       <h4>{project.name}</h4>
+      <p>{project.comments}</p>
+      <p>In this project I used {project.toolsUsed}.</p>
       <button><a href={project.webLink}>Website Link</a></button>
       <button><a href={project.repoLink}>Repo Link</a></button>
     </div>
